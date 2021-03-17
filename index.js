3da132361a9cf386d98e0948491703f0cebf4be2
Michael Zeleniuk <zell.xp@gmail.com>
2021-03-17 14:41:37 +0200

    Create a static cache

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use('/static', express.static(__dirname + '/public'));
 app.use('/static', express.static(__dirname + '/styles'));
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname + '/index.html'));
+    res.sendFile(path.join(__dirname + '/public/index.html'));
 });
 
 app.listen(port, () => {
--- public/serviceWorker.js
+++ public/serviceWorker.js
@@ -1,7 +1,29 @@
-self.addEventListener('install', (event) => {
-    console.log(event);
+const staticCacheName = 'codian-static-v1';
+
+const assetUrls = [
+    '/static/index.html',
+    '/static/app.js',
+    '/static/index.css',
+    '/static/brand.svg',
+    '/static/icon-144-144.png'
+];
+
+self.addEventListener('install', async () => {
+    const cache = await caches.open(staticCacheName);
+    await cache.addAll(assetUrls);
 });
 
-self.addEventListener('activate', (event) => {
-    console.log(event);
+self.addEventListener('activate', async () => {
+    const cacheNames = await caches.keys();
+    await Promise.all(cacheNames.filter((name) => name !== staticCacheName).map((name) => caches.delete(name)));
 });
+
+self.addEventListener('fetch', (event) => {
+    const { request } = event;
+    event.respondWith(cacheFirst(request));
+});
+
+async function cacheFirst(request) {
+    const cached = await caches.match(request);
+    return cached ?? await fetch(request);
+}
